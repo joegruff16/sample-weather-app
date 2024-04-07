35d97eb66fa46ce45813dc6c5d9b35d22730adce
Joseph Ruff <joegruff16@gmail.com>
2024-04-07 18:36:55 -0400

    Linked JS to HTML and began creating variables for the weather form, cityInput, cards, and the apiKey

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "03c9d5245e41ca3d90708da632392882";
+
+weatherForm.addEventListener("submit", event => {
+
+});
