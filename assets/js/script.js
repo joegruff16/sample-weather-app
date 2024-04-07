e4af6ff6dcbc977d25112a335581f0fe7f548b23
Joseph Ruff <joegruff16@gmail.com>
2024-04-07 18:40:32 -0400

    Created functions to get the weather data, to display the weather information, to get the weather emoji, and to display an error message if no city is inputed

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,23 @@ const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
 const apiKey = "03c9d5245e41ca3d90708da632392882";
 
+// Added an event listener to activate the get weather button when submitting the form
 weatherForm.addEventListener("submit", event => {
 
 });
+
+async function getWeatherData(city) {
+
+}
+
+function displayWeatherInfo(data) {
+
+}
+
+function getWeatherEmoji(weatherId) {
+
+}
+
+function displayError(message) {
+
+}
